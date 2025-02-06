a5e002bb3e733a7cebc187a761287d5189b3e926
evilicebergs <voiced.fair-0a@icloud.com>
2025-02-06 17:32:26 +0200

    Initial Commit

---
+++ CookIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookIt
+//
+//  Created by Artem Golovchenko on 2025-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookIt/CookItApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookItApp.swift
+//  CookIt
+//
+//  Created by Artem Golovchenko on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct CookItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
