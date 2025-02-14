cd58997517acaa1cf271b504183bbbe685e8ed10
evilicebergs <voiced.fair-0a@icloud.com>
2025-02-14 15:18:49 +0200

    finish cell view pill, add all needed fonts

--- CookIt/Constants/Constants.swift
+++ CookIt/Constants/Constants.swift
@@ -9,4 +9,9 @@ import Foundation
 
 struct Constants {
     static let randomImage = "https://picsum.photos/600/600"
+    
+    static let appFont: String = "Baloo2"
+    static let appFontBold: String = "Baloo2-Bold"
+    static let appFontMedium: String = "Baloo2-Medium"
+    static let appFontSemiBold: String = "Baloo2-SemiBold"
 }
--- CookIt/Views/Components/MostPopularCellView.swift
+++ CookIt/Views/Components/MostPopularCellView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MostPopularCellView: View {
     
-    var statuses: [String] = ["vegan", "gluten free"]
+    var statuses: [String] = ["vegan", "gluten-free"]
     var cookingTime: (Int, String) = (10, "min")
     var author: String = "John Doe"
     var title: String = "Guacamole"
@@ -21,7 +21,7 @@ struct MostPopularCellView: View {
                 .specialYellow.opacity(0.49)
         case "vegetarian":
                 .specialLightBlue.opacity(0.55)
-        case "gluten free":
+        case "gluten-free":
                 .specialGreen.opacity(0.54)
         case "lactose-free":
                 .specialPink.opacity(0.49)
@@ -42,7 +42,7 @@ struct MostPopularCellView: View {
                     HStack {
                         ForEach(statuses, id: \.self) { status in
                             Text(status)
-                                .font(.custom("Baloo2", size: 10))
+                                .font(.custom(Constants.appFontMedium, size: 10))
                                 .foregroundStyle(.specialBlack)
                                 .padding(.horizontal, 6)
                                 .padding(.vertical, 3)
@@ -54,18 +54,28 @@ struct MostPopularCellView: View {
                     HStack(spacing: 2) {
                         Image(systemName: "clock")
                             .font(.system(size: 14))
+                            .fontWeight(.medium)
                         
                         Text("\(cookingTime.0) \(cookingTime.1)")
-                            .font(.custom("Baloo2", size: 14))
+                            .font(.custom(Constants.appFontMedium, size: 14))
                     }
                     .frame(maxWidth: .infinity, alignment: .trailing)
 
                     .foregroundStyle(.specialWhite)
                 }
                 
-                VStack {
-                    
+                VStack(alignment: .leading, spacing: -8) {
+                    Text(author)
+                        .font(.custom(Constants.appFontMedium, size: 12))
+
+                    Text(title)
+                        .font(.custom(Constants.appFontBold, size: 20))
+                        .fontWeight(.bold)
                 }
+                .foregroundStyle(.specialWhite)
+                .frame(maxHeight: .infinity, alignment: .bottomLeading)
+                .frame(maxWidth: .infinity,alignment: .leading)
+                .offset(y: 8)
             }
             .padding(16)
             .frame(maxWidth: .infinity, alignment: .leading)
